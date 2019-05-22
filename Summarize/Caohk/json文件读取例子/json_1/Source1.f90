6d8200d37d52b3d7f39b018a87695d2f8afe557e
0chk0 <496405242@qq.com>
2019-05-23 05:14:06 +0800

    json文件读取

---
+++ Summarize/Caohk/json文件读取例子/json_1/Source1.f90
@@ -0,0 +1,55 @@
+program jf_test_8
+    use json_module
+    implicit none
+    type(json_file) :: json
+    integer(kind=4) :: i,j,k,ntime,nrec
+    character(len=20) :: str
+    real(kind=4),allocatable,dimension(:):: a,h
+    real(kind=4),allocatable,dimension(:):: s1,c1,trange
+    real(kind=4),allocatable,dimension(:,:):: s,coordrec
+    real(kind=4) :: I0,ndipole
+    character(len=20) :: name1,name2
+    !初始化
+    call json%initialize()
+    !读取文件
+    call json%load_file(filename='shiyan.json')
+    !将文件的内容输出到console上面
+    call json%print_file()
+    !提取文件里面的内容给变量使用json%get
+    !提取里面的整数,用integer即可,kind无所谓
+    call json%get('nlayer',i)
+    !提取里面的数组，直接用未分配的即可，利用json%get会自动为你分配好，也可用分配的
+    !但不管哪个，都要记得在仍要销毁数组
+    call json%get('res',a)
+    !
+    call json%get('thickness',h)
+    call json%get('nsource',j)
+    !提取键值中的子集，这个怎么处理比较好，这里提供个人的小技巧作为参考
+    allocate(s(j,4))  !因为每个源的坐标是4，这是固定死的
+    do k=1,j,1
+        write(str,*) k
+        str='source.source'//trim(adjustl(str)) !得到键值
+        call json%get(str,s1)
+        s(k,:)=s1
+    enddo
+    !
+    call json%get('current',I0)
+    call json%get('ndipole',ndipole)
+    call json%get('ntime',ntime)
+    call json%get('trange',trange)
+    call json%get('nrec',nrec)
+    allocate(coordrec(nrec,3))  !因为每个源的坐标是4，这是固定死的
+    do k=1,nrec,1
+        write(str,*) k
+        str='coordrec.rec'//trim(adjustl(str)) !得到键值
+        call json%get(str,c1)
+        coordrec(k,:)=c1
+    enddo
+    !提取文字部分，怎么读取文字部分
+    call json%load_from_string(name1)
+    write(*,*) name1
+
+    pause
+    
+end program jf_test_8
+
---
+++ Summarize/Caohk/json文件读取例子/json_1/Source2.f90
@@ -0,0 +1,123 @@
+module jf_test_8_mod
+
+    use json_module, IK => json_IK
+    use, intrinsic :: iso_fortran_env , only: error_unit, output_unit
+
+    implicit none
+
+    private
+    public :: test_8
+
+contains
+
+    subroutine test_8(error_cnt)
+
+    !! read a JSON structure from a string
+
+    implicit none
+
+    integer,intent(out) :: error_cnt
+
+    type(json_value),pointer :: p
+    type(json_core) :: json       !! factory for manipulating `json_value` pointers
+
+    character(len=*),parameter :: newline = achar(10)
+
+    character(len=*),parameter :: str = '{ "label": "foo",'//newline//' "value": "bar" }'
+
+    character(len=*),parameter :: str2 = '{ "label": "foo",'//newline//&
+                                         '  "value": "bar",'//newline//&
+                                         '  "empty_array": [],'//newline//&
+                                         '  "empty_object": {}' //newline//&
+                                         '}'
+
+    character(len=*),parameter :: str_invalid = '{ "label": "foo",'//newline//' "value : "bar" }'
+
+    error_cnt = 0
+    call json%initialize(allow_duplicate_keys=.false.)
+    if (json%failed()) then
+        call json%print_error_message(error_unit)
+        error_cnt = error_cnt + 1
+    end if
+
+    write(error_unit,'(A)') ''
+    write(error_unit,'(A)') '================================='
+    write(error_unit,'(A)') '   EXAMPLE 8 : read JSON from string'
+    write(error_unit,'(A)') '================================='
+    write(error_unit,'(A)') ''
+
+    write(error_unit,'(A)') '**************'
+    write(error_unit,'(A)') ' Valid test 1:'
+    write(error_unit,'(A)') '**************'
+    write(error_unit,'(A)') ''
+    call json%parse(str=str, p=p)   ! read it from str
+    if (json%failed()) then
+        call json%print_error_message(error_unit)
+        error_cnt = error_cnt + 1
+    end if
+    write(output_unit,'(A)') '{ "part a" : '
+    call json%print(p,int(output_unit,IK))  ! print to console
+    write(output_unit,'(A)') ','
+    if (json%failed()) then
+        call json%print_error_message(error_unit)
+        error_cnt = error_cnt + 1
+    end if
+    call json%destroy(p)            ! cleanup
+    if (json%failed()) then
+        call json%print_error_message(error_unit)
+        error_cnt = error_cnt + 1
+    end if
+    write(error_unit,'(A)') ''
+
+    write(error_unit,'(A)') '**************'
+    write(error_unit,'(A)') ' Valid test 2:'
+    write(error_unit,'(A)') '**************'
+    write(error_unit,'(A)') ''
+    call json%parse(str=str2, p=p)   ! read it from str
+    if (json%failed()) then
+        call json%print_error_message(error_unit)
+        error_cnt = error_cnt + 1
+    end if
+    write(output_unit,'(A)') '"part b" : '
+    call json%print(p,int(output_unit,IK))  ! print to console
+    write(output_unit,'(A)') ','
+    if (json%failed()) then
+        call json%print_error_message(error_unit)
+        error_cnt = error_cnt + 1
+    end if
+    call json%destroy(p)            ! cleanup
+    if (json%failed()) then
+        call json%print_error_message(error_unit)
+        error_cnt = error_cnt + 1
+    end if
+    write(error_unit,'(A)') ''
+
+    write(error_unit,'(A)') '**************'
+    write(error_unit,'(A)') ' Invalid test:'
+    write(error_unit,'(A)') '**************'
+    write(error_unit,'(A)') ''
+    call json%parse(str=str_invalid, p=p)   ! read it from str
+    if (json%failed()) then
+        call json%print_error_message(error_unit)
+    else
+        write(error_unit,'(A)') 'This should have failed!'
+        error_cnt = error_cnt + 1
+    end if
+    write(output_unit,'(A)') '"part c" : '
+    call json%print(p,int(output_unit,IK))  ! print to console
+    write(output_unit,'(A)') '}'
+    if (json%failed()) then
+        call json%print_error_message(error_unit)
+        error_cnt = error_cnt + 1
+    end if
+    call json%destroy(p)            ! cleanup
+    if (json%failed()) then
+        call json%print_error_message(error_unit)
+        error_cnt = error_cnt + 1
+    end if
+    write(error_unit,'(A)') ''
+
+    end subroutine test_8
+
+end module jf_test_8_mod
+!*****************************************************************************************
